180278c4a2fc893da4b2d1375569d579c8473e68
Priyanshu A <priyanshua@Priyanshus-MacBook-Pro.local>
2026-03-05 09:56:47 +0530

    creates a function to render specified type pokemon

--- pokedex_with_frontend_js/public/script.js
+++ pokedex_with_frontend_js/public/script.js
@@ -1,32 +1,33 @@
 import { createFragment } from "./fragment.js";
 
-const fetchPokemon = async () => 
+const fetchPokemon = async () =>
   fetch("/data/all_data.json")
-  .then(data => data.json());
+    .then((data) => data.json());
 
-const createCard = ({stats, name, types, url}) => {
+const createCard = ({ stats, name, types, url }) => {
   const cardformat = [
-    "div", {class: "card"},
-      ["div", {class: "image"}, ["img", {src: url}, ""]],
-      ["div", {class: "details"}, 
-        ["div", {class: "overview"}, 
-          ["div", {class: "name"}, name],
-          ["div", {class: "type-container"}, 
-            ...types.map(type => ["div", {class: `${type.toLowerCase()} type`}, type])],
-        ],
-        
-        ["table", {}, 
-          ["tbody", {}, 
-            ...stats.map(({base_stat, name}) => 
-              ["tr", {}, ["td", {}, name], ["td", {}, `${base_stat}`]]
-          )
-          ]
-        ]
-      ],
+    "div",
+    { class: "card" },
+    ["div", { class: "image" }, ["img", { src: url }, ""]],
+    ["div", { class: "details" }, ["div", { class: "overview" }, ["div", {
+      class: "name",
+    }, name], [
+      "div",
+      { class: "type-container" },
+      ...types.map(
+        (type) => ["div", { class: `${type.toLowerCase()} type` }, type]
+      ),
+    ]], ["table", {}, [
+      "tbody",
+      {},
+      ...stats.map((
+        { base_stat, name },
+      ) => ["tr", {}, ["td", {}, name], ["td", {}, `${base_stat}`]]),
+    ]]],
   ];
 
   return createFragment(cardformat);
-}
+};
 
 const createAllCard = (allPokemon) => {
   const main = document.querySelector("main");
@@ -36,37 +37,43 @@ const createAllCard = (allPokemon) => {
 
 const extractAllTypesName = (allPokemon) => {
   const typeList = [];
-  allPokemon.forEach(({types}) => {
-    types.forEach(type => {
-      if(!typeList.includes(type)) typeList.push(type);
-    })
+  allPokemon.forEach(({ types }) => {
+    types.forEach((type) => {
+      if (!typeList.includes(type)) typeList.push(type);
+    });
   });
   return typeList;
-}
- 
+};
+
 const createHeader = (allPokemon) => {
   const allTypes = extractAllTypesName(allPokemon);
   const headingFormat = ["h1", {}, "Pokemon Gallery"];
   const searchBarFormat = [
-    "form", {class: "search-bar", method: "get"},
-      ["input", {
-        list: "options",
-        type: "text",
-        placeholder: "enter pokemon name",
-        name: "name"
-      }, ""],
-      ["dataList", {id: "options"}, 
-        ...allPokemon.map(({name}) => ["option", {value: name}, ""])
-      ],
-      ["button", {}, "search"]
-    ];
+    "form",
+    { class: "search-bar", method: "get" },
+    ["input", {
+      list: "options",
+      type: "text",
+      placeholder: "enter pokemon name",
+      name: "name",
+    }, ""],
+    [
+      "dataList",
+      { id: "options" },
+      ...allPokemon.map(({ name }) => ["option", { value: name }, ""]),
+    ],
+    ["button", {}, "search"],
+  ];
 
   const typeFormat = [
-    "form", {class: "type-selector"}, 
-    ["select", {name: "type", onchange: "this.form.submit()"}, 
+    "form",
+    { class: "type-selector" },
+    [
+      "select",
+      { name: "type" },
       ["option", {}, "select type"],
-      ...allTypes.map(type => ["option", {value: type}, type])
-    ]
+      ...allTypes.map((type) => ["option", { value: type }, type]),
+    ],
   ];
 
   const header = document.querySelector("header");
@@ -74,9 +81,26 @@ const createHeader = (allPokemon) => {
   const searchBar = createFragment(searchBarFormat);
   const typeSelector = createFragment(typeFormat);
   header.append(heading, searchBar, typeSelector);
-}
+};
 
-window.onload = () => {
-  fetchPokemon().then(createAllCard);
-  fetchPokemon().then(createHeader);
-}
+const createTypeCard = (type, allPokemon) => {
+  const allSpecifiedTypePokemon = allPokemon.filter(({ types }) =>
+    types.includes(type)
+  );
+  const pokemonContainer = document.querySelector(".container");
+  pokemonContainer.textContent = "";
+  createAllCard(allSpecifiedTypePokemon)
+};
+
+window.onload = async () => {
+  const allPokemon = await fetchPokemon();
+  createHeader(allPokemon);
+  createAllCard(allPokemon);
+
+  const typeSelector = document.querySelector("select");
+  typeSelector.addEventListener("change", (event) => {
+    event.preventDefault();
+    const selectedType = event.target.value;
+    createTypeCard(selectedType, allPokemon);
+  });
+};
